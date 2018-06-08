aa2604cbf0948d5f7cdc42acbc93eedcc356e317
wangzhicheng <wangzhicheng@uworks.cc>
2018-06-08 09:41:54 +0800

    提交修改

---
+++ changcheng-sso/src/main/java/site/changcheng/sso/app.java
@@ -0,0 +1,8 @@
+package site.changcheng.sso;
+
+@SpringBootApplication
+public class App {
+  public static void main(String[] args) {
+    SpringApplication.run(ConsumerApplication.class, args);
+  }
+}
