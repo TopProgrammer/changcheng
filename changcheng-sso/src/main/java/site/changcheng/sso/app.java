14c24e71ea2f9733eb0a16f4284cc353b6f54aa6
wangzhicheng <wangzhicheng@uworks.cc>
2018-07-05 15:41:14 +0800

    聚合模块，引入Springboot，搭建完成

--- changcheng-sso/src/main/java/site/changcheng/sso/app.java
+++ changcheng-sso/src/main/java/site/changcheng/sso/app.java
@@ -1,8 +1,12 @@
 package site.changcheng.sso;
 
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
 @SpringBootApplication
 public class App {
   public static void main(String[] args) {
-    SpringApplication.run(ConsumerApplication.class, args);
+    SpringApplication.run(App.class, args);
   }
 }
+
