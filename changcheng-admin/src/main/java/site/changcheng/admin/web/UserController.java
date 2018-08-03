958792057d2546165912035d9974aad45d8563de
wangzhicheng <wangzhicheng@uworks.cc>
2018-08-03 21:18:56 +0800

    添加图形验证码，，添加CROS过滤器，登陆过滤器改为登陆拦截器

--- changcheng-admin/src/main/java/site/changcheng/admin/web/UserController.java
+++ changcheng-admin/src/main/java/site/changcheng/admin/web/UserController.java
@@ -3,9 +3,7 @@ package site.changcheng.admin.web;
 import io.swagger.annotations.Api;
 import io.swagger.annotations.ApiOperation;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 import site.changcheng.admin.Service.SSOService;
 import site.changcheng.admin.web.request.LoginForm;
 
@@ -13,19 +11,13 @@ import javax.annotation.Resource;
 import javax.validation.Valid;
 
 @RestController
-@RequestMapping(value="/user")
-@Api
+@RequestMapping(value = "/user")
+@Api(value = "用户中心接口")
+@CrossOrigin
 public class UserController extends BaseController {
 
-  @Resource
-  private SSOService ssoService;
-  @RequestMapping("/login")
-  @ApiOperation("登陆")
-  public ResponseEntity<String> login(@Valid @RequestBody LoginForm form){
-    ssoService.checkLoginState(form);
 
-    LOG.info("===============================");
 
-    return ResponseEntity.ok("SUCCESS");
-  }
+
+
 }
--- changcheng-sso/src/main/java/site/changcheng/sso/web/UserController.java
+++ changcheng-sso/src/main/java/site/changcheng/sso/web/UserController.java
@@ -1,19 +1,49 @@
 package site.changcheng.sso.web;
 
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
+import site.changcheng.sso.constants.Constants;
 import site.changcheng.sso.web.request.LoginForm;
 
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.HttpSession;
 import javax.validation.Valid;
+import java.util.HashMap;
+import java.util.Map;
 
 @RestController
-@RequestMapping(value="/user")
-public class UserController {
+@RequestMapping(value = "/user")
+public class UserController extends BaseController{
 
-  @RequestMapping("/login")
-  public ResponseEntity<String> login(@Valid @RequestBody LoginForm form){
-    return ResponseEntity.ok("SUCCESS");
+  @RequestMapping(value = "/login",method = RequestMethod.POST)
+  public ResponseEntity<String> login(@Valid @RequestBody LoginForm form,HttpServletRequest request) {
+
+    String pwd = form.getPassword();
+    String picCode = form.getPicCaptcha();
+    String userName = form.getUsername();
+
+    // 校验验证码
+    HttpSession session = request.getSession(false);
+    if (session == null) {
+      // 抛异常
+    }
+    String captchaCache = (String) session
+        .getAttribute(Constants.WEB_LOGIN_IMAGE_CODE);
+    if (captchaCache == null) {
+      // 抛异常
+
+    }
+
+    if (!(picCode.trim()).equalsIgnoreCase(captchaCache)) {
+      // 抛异常
+    }
+
+    LOG.info("=========图形验证码校验成功==开始登陆校验登陆=============");
+//    request.getSession().setAttribute("user", user);
+    Map<String, Object> returnData = new HashMap<String, Object>();
+    //session.removeAttribute(Constants.WEB_LOGIN_IMAGE_CODE);
+    return ResponseEntity.ok().body("Success");
   }
+
 }
